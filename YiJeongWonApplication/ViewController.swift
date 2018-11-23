221ba19e970cf1670598f91faa4033d040261093
JEONG-WON YI <raccoongury@icloud.com>
2018-11-24 01:56:10 +0900

    Initial Commit

---
+++ YiJeongWonApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YiJeongWonApplication
+//
+//  Created by JEONG-WON YI on 24/11/2018.
+//  Copyright © 2018 the. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
